e3a827507f59caccd27b1c20b4b0440a8aae2dd4
akhsanby <akhsanby@gmail.com>
2023-11-28 20:09:46 +0700

    refactoring code

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,3 +1,12 @@
+import Head from "next/head";
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>User Contact</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
