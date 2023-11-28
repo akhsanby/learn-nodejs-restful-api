6311141e98033311904f58b4efb14c63168fd412
akhsanby <akhsanby@gmail.com>
2023-11-28 20:10:56 +0700

    update log info

--- server/src/main.js
+++ server/src/main.js
@@ -2,5 +2,5 @@ import { web } from "./app/web.js";
 import { logger } from "./app/logging.js";
 
 web.listen(8000, () => {
-  logger.info("App start");
+  logger.info("App start in http://localhost:8000");
 });
