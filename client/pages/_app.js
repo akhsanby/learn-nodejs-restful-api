a0b8b31353c733534c705cf9b492f71b7462bf7e
akhsanby <akhsanby@gmail.com>
2023-12-03 11:50:49 +0700

    add safe routes & redux store

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,4 +1,8 @@
 import Head from "next/head";
+import nookies from "nookies";
+import { getLoggedInUser } from "@/utils/axios-client.js";
+import { store } from "@/utils/redux/store/index.js";
+import { Provider } from "react-redux";
 
 export default function App({ Component, pageProps }) {
   return (
@@ -6,7 +10,26 @@ export default function App({ Component, pageProps }) {
       <Head>
         <title>User Contact</title>
       </Head>
-      <Component {...pageProps} />
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
     </>
   );
+}
+
+export async function getServerSideProps(context) {
+  const cookies = nookies.get(context);
+  const isAuthenticated = await getLoggedInUser();
+
+  if (cookies.token || isAuthenticated) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: "/home",
+      },
+      props: {},
+    };
+  }
+
+  return { props: {} };
 }
